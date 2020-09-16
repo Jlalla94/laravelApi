93be2420be318b528774019b1a8e8aa9a582e248
Kyryll <lapkir94@gmail.com>
2020-09-16 08:03:19 +0300

    admin lte

---
+++ api/app/Http/Controllers/FormController.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+use Illuminate\Support\Facades\Mail;
+
+class FormController extends Controller
+{
+    public function faqForm(Request $mailRequest)
+    {
+        $data = [
+            'fullName' => $mailRequest->fullName,
+            'phone' => $mailRequest->phone,
+            'email' => $mailRequest->email,
+            'company' => $mailRequest->company,
+            'city' => $mailRequest->city,
+        ];
+
+        Mail::send('emails.land.admin', $data, function ($message)  {
+            $message->to(config('mail.username'))->subject('From Get Price');
+        });
+
+        return response()->json(['message' => 'Mail sent successfully'], Response::HTTP_OK);
+    }
+    public function priceForm(Request $mailRequest)
+    {
+        $data = [
+            'fullName' => $mailRequest->fullName,
+            'phone' => $mailRequest->phone,
+            'email' => $mailRequest->email,
+            'company' => $mailRequest->company,
+            'city' => $mailRequest->city,
+        ];
+
+        Mail::send('emails.land.admin', $data, function ($message)  {
+            $message->to(config('mail.username'))->subject('From Get Price');
+        });
+
+        return response()->json(['message' => 'Mail sent successfully'], Response::HTTP_OK);
+    }
+
+    public function sendCommercialPrice(Request $mailRequest)
+    {
+        $data = [
+            'fullName' => $mailRequest->fullName,
+            'phone' => $mailRequest->phone,
+            'email' => $mailRequest->email,
+            'company' => $mailRequest->company,
+            'city' => $mailRequest->city,
+        ];
+
+        Mail::send('emails.land.admin', $data, function ($message)  {
+            $message->to(config('mail.username'))->subject('From Get Price');
+        });
+
+        return response()->json(['message' => 'Mail sent successfully'], Response::HTTP_OK);
+    }
+}
---
+++ api/resources/views/emails/faq.blade.php
@@ -0,0 +1,18 @@
+<h2>Information</h2>
+<ul>
+    <li>
+        Name: <strong>{{$fullName}}</strong>
+    </li>
+    <li>
+        Email: <strong>{{$email}}</strong>
+    </li>
+    <li>
+        Phone: <strong>{{$phone}}</strong>
+    </li>
+    <li>
+        Company: <strong>{{$company}}</strong>
+    </li>
+    <li>
+        City: <strong>{{$city}}</strong>
+    </li>
+</ul>
