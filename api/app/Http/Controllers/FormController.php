7b1813c2fa1fb7015353162fdcbcdb890f8acd91
Kyryll <lapkir94@gmail.com>
2020-09-16 09:03:41 +0300

    admin lte

--- api/app/Http/Controllers/FormController.php
+++ api/app/Http/Controllers/FormController.php
@@ -36,7 +36,7 @@ class FormController extends Controller
         ];
 
         Mail::send('emails.faq', $data, function ($message)  {
-            $message->to(config('mail.username'))->subject('From Get Price');
+            $message->to('test@cookies.com.ua')->subject('From Get Price');
         });
         return response()->json(['message' => 'Mail sent successfully'], Response::HTTP_OK);
     }
