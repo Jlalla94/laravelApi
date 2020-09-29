a3255e86dced7efddfbcad7b2431b106d9f5f335
Kyryll <lapkir94@gmail.com>
2020-09-29 08:06:42 +0300

    fixes

--- api/resources/views/product/create.blade.php
+++ api/resources/views/product/create.blade.php
@@ -5,62 +5,55 @@
     {!!Form::open(['url' => route('product.create'), 'files' => true])->post()!!}
 
 
+    <h1>Продукт</h1>
 
-        <h1>Продукт</h1>
+    <div class="form-group">
+        {!! Form::label('title', 'Титл:', ['class' => 'col-lg-2 control-label']) !!}
+            {!!Form::text('title', 'Титл')->placeholder('Название продукта')->lg()->required()!!}
+    </div>
 
-        <!-- Email -->
-        <div class="form-group">
-            {!! Form::label('title', 'Титл:', ['class' => 'col-lg-2 control-label']) !!}
-            <div class="col-lg-10">
-                {!!Form::text('title', 'Титл')->placeholder('Название продукта')->lg()->required()!!}
-            </div>
+    <div class="form-group">
+        {!! Form::label('slug', 'Ссылка:', ['class' => 'col-lg-2 control-label']) !!}
+        <div class="col-lg-10">
+            {!!Form::text('slug', 'Ссылка')->placeholder('Урл продукта')->lg()->required()!!}
         </div>
+    </div>
 
-        <!-- Password -->
-        <div class="form-group">
-            {!! Form::label('slug', 'Ссылка:', ['class' => 'col-lg-2 control-label']) !!}
-            <div class="col-lg-10">
-                {!!Form::text('slug', 'Ссылка')->placeholder('Урл продукта')->lg()->required()!!}
-
-            </div>
+    <div class="form-group">
+        {!! Form::label('textarea', 'Textarea', ['class' => 'col-lg-2 control-label']) !!}
+        <div class="col-lg-10">
+            {!! Form::textarea('textarea', $value = null, ['class' => 'form-control', 'rows' => 3]) !!}
+            {!!Form::textarea('text', null,['class' => 'form-control', 'rows' => 3])->placeholder('Описание продукта')->lg()->required()!!}
         </div>
+    </div>
 
-        <!-- Text Area -->
-        <div class="form-group">
-            {!! Form::label('textarea', 'Textarea', ['class' => 'col-lg-2 control-label']) !!}
-            <div class="col-lg-10">
-                {!! Form::textarea('textarea', $value = null, ['class' => 'form-control', 'rows' => 3]) !!}
-                {!!Form::textarea('text', null,['class' => 'form-control', 'rows' => 3])->placeholder('Описание продукта')->lg()->required()!!}
-            </div>
+    <div class="form-group">
+        {!! Form::label('expiration_date', 'Термин хранения в сутках:', ['class' => 'col-lg-2 control-label']) !!}
+        <div class="col-lg-10">
+            {!!Form::text('expiration_date', 'Термин')->lg()->required()->type('number')!!}
+
         </div>
+    </div>
+    <div class="form-group">
+        {!! Form::label('packaging_option', 'Варианты упаковки:', ['class' => 'col-lg-2 control-label']) !!}
+        <div class="col-lg-10">
+            {!!Form::text('packaging_option', 'Варианты упаковки')->placeholder('Варианты упаковки')->lg()->required()!!}
 
-        <div class="form-group">
-            {!! Form::label('expiration_date', 'Термин хранения в сутках:', ['class' => 'col-lg-2 control-label']) !!}
-            <div class="col-lg-10">
-                {!!Form::text('expiration_date', 'Термин')->lg()->required()->type('number')!!}
-
-            </div>
         </div>
-        <div class="form-group">
-            {!! Form::label('packaging_option', 'Варианты упаковки:', ['class' => 'col-lg-2 control-label']) !!}
-            <div class="col-lg-10">
-                {!!Form::text('packaging_option', 'Варианты упаковки')->placeholder('Варианты упаковки')->lg()->required()!!}
+    </div>
+    <div class="form-group">
+        {!! Form::label('image', 'Картинка:', ['class' => 'col-lg-2 control-label']) !!}
+        <div class="col-lg-10">
+            {!!Form::file('image')->lg()->required()!!}
 
-            </div>
         </div>
-        <div class="form-group">
-            {!! Form::label('image', 'Картинка:', ['class' => 'col-lg-2 control-label']) !!}
-            <div class="col-lg-10">
-                {!!Form::file('image')->lg()->required()!!}
+    </div>
 
-            </div>
+    <div class="form-group">
+        <div class="col-lg-10 col-lg-offset-2">
+            {!! Form::submit('Сохранить', ['class' => 'btn btn-lg btn-info pull-right'] ) !!}
         </div>
-
-        <div class="form-group">
-            <div class="col-lg-10 col-lg-offset-2">
-                {!! Form::submit('Сохранить', ['class' => 'btn btn-lg btn-info pull-right'] ) !!}
-            </div>
-        </div>
+    </div>
 
 
     {!! Form::close()  !!}
