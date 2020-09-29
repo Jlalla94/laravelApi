308134aa44304f7874a7865ff97d2abfe2e1f757
Kyryll <lapkir94@gmail.com>
2020-09-29 08:17:51 +0300

    fixes

--- api/resources/views/product/create.blade.php
+++ api/resources/views/product/create.blade.php
@@ -23,41 +23,35 @@
     <h1>Продукт</h1>
 
     <div class="form-group">
-        {{ Form::text('title', 'title')->placeholder('Название продукта')->lg()->required()->label( 'Титл:') }}
+        {!! Form::text('title', 'title')->placeholder('Название продукта')->lg()->required()->label( 'Титл:') !!}
 
     </div>
 
     <div class="form-group">
-        {!! Form::label('slug', 'Ссылка:', ['class' => 'col-lg-2 control-label']) !!}
         <div class="col-lg-10">
             {!! Form::text('slug', 'Ссылка')->placeholder('Урл продукта')->lg()->required() !!}
         </div>
     </div>
 
     <div class="form-group">
-        {!! Form::label('textarea', 'Textarea', ['class' => 'col-lg-2 control-label']) !!}
         <div class="col-lg-10">
-            {!! Form::textarea('textarea', $value = null, ['class' => 'form-control', 'rows' => 3]) !!}
             {!!Form::textarea('text', null,['class' => 'form-control', 'rows' => 3])->placeholder('Описание продукта')->lg()->required()!!}
         </div>
     </div>
 
     <div class="form-group">
-        {!! Form::label('expiration_date', 'Термин хранения в сутках:', ['class' => 'col-lg-2 control-label']) !!}
         <div class="col-lg-10">
             {!!Form::text('expiration_date', 'Термин')->lg()->required()->type('number')!!}
 
         </div>
     </div>
     <div class="form-group">
-        {!! Form::label('packaging_option', 'Варианты упаковки:', ['class' => 'col-lg-2 control-label']) !!}
         <div class="col-lg-10">
             {!!Form::text('packaging_option', 'Варианты упаковки')->placeholder('Варианты упаковки')->lg()->required()!!}
 
         </div>
     </div>
     <div class="form-group">
-        {!! Form::label('image', 'Картинка:', ['class' => 'col-lg-2 control-label']) !!}
         <div class="col-lg-10">
             {!!Form::file('image')->lg()->required()!!}
 
