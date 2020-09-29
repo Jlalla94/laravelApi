105891c9f9f8d9758c04ca13fc296a6b1a858585
Kyryll <lapkir94@gmail.com>
2020-09-29 08:12:39 +0300

    fixes

--- api/resources/views/product/create.blade.php
+++ api/resources/views/product/create.blade.php
@@ -20,11 +20,11 @@
 
     {!!Form::open(['url' => route('product.create'), 'files' => true])->post()!!}
 
-
     <h1>Продукт</h1>
 
     <div class="form-group">
         {!! Form::label('title', 'Титл:', ['class' => 'col-lg-2 control-label']) !!}
+        {!! Form::text('title', 'title')->placeholder('Название продукта')->lg()->required() !!}
 
     </div>
 
