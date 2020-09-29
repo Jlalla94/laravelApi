84487650c770afeb45225f6d710e014ba8ed5458
Kyryll <lapkir94@gmail.com>
2020-09-29 08:03:28 +0300

    fixes

--- api/resources/views/product/create.blade.php
+++ api/resources/views/product/create.blade.php
@@ -1,22 +1,72 @@
 <html>
 <body>
+<div class="well">
+
+    {!!Form::open(['url' => route('product.create'), 'files' => true])->post()!!}
 
 
-{!!Form::open(['url' => route('product.create'), 'files' => true])->post()!!}
+    <fieldset>
 
-{!!Form::text('title', 'Титл')->placeholder('Название продукта')->lg()->required()!!}
+        <legend>Продукт</legend>
 
-{!!Form::text('slug', 'Ссылка')->placeholder('Урл продукта')->lg()->required()!!}
+        <!-- Email -->
+        <div class="form-group">
+            {!! Form::label('title', 'Титл:', ['class' => 'col-lg-2 control-label']) !!}
+            <div class="col-lg-10">
+                {!!Form::text('title', 'Титл')->placeholder('Название продукта')->lg()->required()!!}
+            </div>
+        </div>
 
-{!!Form::text('text', 'Описание')->placeholder('Описание продукта')->lg()->required()!!}
+        <!-- Password -->
+        <div class="form-group">
+            {!! Form::label('slug', 'Ссылка:', ['class' => 'col-lg-2 control-label']) !!}
+            <div class="col-lg-10">
+                {!!Form::text('slug', 'Ссылка')->placeholder('Урл продукта')->lg()->required()!!}
 
-{!!Form::text('expiration_date', 'Термин')->placeholder('Термин хранения в сутках')->lg()->required()->type('number')!!}
+            </div>
+        </div>
 
-{!!Form::text('packaging_option', 'Варианты упаковки')->placeholder('Варианты упаковки')->lg()->required()!!}
+        <!-- Text Area -->
+        <div class="form-group">
+            {!! Form::label('textarea', 'Textarea', ['class' => 'col-lg-2 control-label']) !!}
+            <div class="col-lg-10">
+                {!! Form::textarea('textarea', $value = null, ['class' => 'form-control', 'rows' => 3]) !!}
+                {!!Form::textarea('text', null,['class' => 'form-control', 'rows' => 3])->placeholder('Описание продукта')->lg()->required()!!}
+            </div>
+        </div>
 
-{!!Form::file('image')->placeholder('Картинка')->lg()->required()!!}
+        <div class="form-group">
+            {!! Form::label('expiration_date', 'Термин хранения в сутках:', ['class' => 'col-lg-2 control-label']) !!}
+            <div class="col-lg-10">
+                {!!Form::text('expiration_date', 'Термин')->lg()->required()->type('number')!!}
 
-{!!Form::close()!!}
+            </div>
+        </div>
+        <div class="form-group">
+            {!! Form::label('packaging_option', 'Варианты упаковки:', ['class' => 'col-lg-2 control-label']) !!}
+            <div class="col-lg-10">
+                {!!Form::text('packaging_option', 'Варианты упаковки')->placeholder('Варианты упаковки')->lg()->required()!!}
 
+            </div>
+        </div>
+        <div class="form-group">
+            {!! Form::label('image', 'Картинка:', ['class' => 'col-lg-2 control-label']) !!}
+            <div class="col-lg-10">
+                {!!Form::file('image')->lg()->required()!!}
+
+            </div>
+        </div>
+
+        <div class="form-group">
+            <div class="col-lg-10 col-lg-offset-2">
+                {!! Form::submit('Сохранить', ['class' => 'btn btn-lg btn-info pull-right'] ) !!}
+            </div>
+        </div>
+
+    </fieldset>
+
+    {!! Form::close()  !!}
+
+</div>
 </body>
 </html>
